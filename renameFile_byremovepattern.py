a4afd51d36b005a09a666f1fd9b0b1783437d0d4
Abhishek Shinde <44555258+InquisitiveAS@users.noreply.github.com>
2022-05-03 12:48:08 -0400

    Batch Rename Files by removing string Pattern
    This is a Python Script used for renaming File revisions received for Facade sub-assembly from subcontractors

---
+++ renameFile_byremovepattern.py
@@ -0,0 +1,45 @@
+__author__ = "Abhishek Shinde"
+__copyright__ = "Island Exterior Fabricators LLC | EXD-AS"
+__contact__ = "arabhishek1091@gmail.com | ashinde@islandcompanies.com"
+
+"""
+GOAL OF THE SCRIPT:Rename DXF for Facade Sub-assemblies received from subcontractors.
+Batch Replace a string of characters from File Name's inside a folder ie Batch Renaming Files in Python
+This is useful for renaming the DXF,DWG,PDF,STP,etc
+
+This script will be integrated inside another Python Script for FRM Member Drawings
+"AutoCreatesubfolders_n_movefilesndrenameRegex.py" - Work in progress
+    
+"""
+
+
+import os
+
+folderpath = "C:\\Users\\ashinde\\OneDrive - Island International Industries\\Documents\\Abhishek_Shinde_IEF\\Level 9M\\SST\\DXF"
+
+filesin_folder = os.listdir(folderpath)
+newfilenames = []
+originalfilenames_withpath = []
+newfilenames_withpath=[]
+
+count = 1 
+
+for f in filesin_folder:
+    fn_withpath = os.path.join(folderpath,f)
+    #.replace() is where you can change the file name pattern with names
+    nfn = f.replace('_Flat.dxf','.dxf')
+    nfn_withpath = os.path.join(folderpath,nfn)
+    originalfilenames_withpath.append(fn_withpath)
+    newfilenames.append(nfn)
+    newfilenames_withpath.append(nfn_withpath)
+    os.rename(fn_withpath,nfn_withpath)
+    count += 1
+    
+
+
+print('\n')
+#print(originalfilenames_withpath)
+print('\n')
+#print(newfilenames_withpath)
+
+
